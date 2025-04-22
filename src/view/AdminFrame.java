ee1e04a410d53d420d2def11e7df265b3abd42f0
Phong Hoang Thien <22130264@st.hcmuaf.edu.vn>
2025-04-22 23:11:10 +0700

    initial files

--- src/view/AdminFrame.java
+++ src/view/AdminFrame.java
@@ -1,5 +1,14 @@
 package view;
 
-public class AdminFrame {
+import java.awt.GridLayout;
 
+import javax.swing.JFrame;
+
+public class AdminFrame extends JFrame {
+	public AdminFrame() {
+		setLayout(new GridLayout(1, 1, 5, 5));
+		setSize(1200, 800);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
