67ec526beb00a875386f4b19813790683d75e12d
Phong Hoang Thien <22130264@st.hcmuaf.edu.vn>
2025-05-07 10:33:03 +0700

    Dao refactoring

--- src/Dao/AccountDao.java
+++ src/Dao/AccountDao.java
@@ -1,5 +1,118 @@
 package Dao;
 
-public class AccountDao {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import net_management.DatabaseConnection;
+
+public class AccountDao {
+	private DatabaseConnection db;
+
+	public AccountDao() {
+		this.db = new DatabaseConnection();
+	}
+	
+	public boolean CreateAcc(String uid, String u_pass, int time) {
+		// todo
+		return false;
+	}
+
+	public int GetUserTime(String uid) {
+		// todo
+		return 0;
+	}
+
+	public boolean CheckUPass(String uid, String pw) {
+		// todo
+		return false;
+	}
+
+	public boolean GetUState(String uid) {
+		// todo
+		return false;
+	}
+
+	public boolean UpdatePassword(String uid, String pw) {
+		// todo
+		return false;
+	}
+
+	public boolean UpdateUTime(String uid) {
+		// todo
+		return false;
+	}
+
+	public void SetUState(String uid) {
+		// todo
+	}
+
+	public void ResetUState(String uid) {
+		// todo
+	}
+
+	public boolean AddTimeU(String uid, int time) {
+		// todo
+		return false;
+	}
+
+	public List<String> GetAllUid() {
+		String sq = "SELECT id FROM accounts";
+		try {
+			Connection conn = db.getConnection();
+			Statement s = conn.createStatement();
+			ResultSet rs = s.executeQuery(sq);
+			List<String> list = new ArrayList<String>();
+			while (rs.next()) {
+				list.add(rs.getString(1));
+			}
+			s.close();
+			rs.close();
+			conn.close();
+			return list;
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public boolean SetStartDate(String uid, String date) {
+		// todo
+		return false;
+	}
+
+	public boolean SetEndDate(String uid, String date) {
+		// todo
+		return false;
+	}
+
+	public String GetStartDate(String uid) {
+		// todo
+		return "";
+	}
+
+	public String GetEndDate(String uid) {
+		// todo
+		return "";
+	}
+
+	public void AddIncome(float income, String date) {
+		// todo
+	}
+
+	public void UpdateIncome(float money, String date) {
+		// todo
+	}
+
+	public boolean CheckDateExist(String date) {
+		// todo
+		return false;
+	}
+	
+	
 }
--- src/view/PCManagerPanel.java
+++ src/view/PCManagerPanel.java
@@ -1,17 +1,46 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+
+import ultilz.Constants;
 
 public class PCManagerPanel {
+	private JFrame jf;
 	private JTabbedPane jtp;
+	private JPanel pc_pn; // pc manager panel
+	private JPanel cre_pn; // create account panel
+	private JPanel pay_pn; // nap tien panel
+	private JPanel pwd_ch; // change password panel
+	
 	private DefaultTableModel defaultTable_pc;
 	private JTable pctb; // pc manager table
 	private JDialog jd; // dialog for login dialog
@@ -23,9 +52,153 @@ public class PCManagerPanel {
 	private JPanel addPC_pn;
 	private JButton delPC;
 
-	public PCManagerPanel() {
+	public PCManagerPanel(JFrame jf, JTabbedPane jtp) {
+		this.jf = jf;
+		this.jtp = jtp;
 		init();
 	}
 
-	private void init() {}
+	private void init() {
+		String pc_cols[] = { "Máy khách", "Id Người dùng", "Trạng thái", "Thời gian", "Còn lại" };
+		defaultTable_pc = new DefaultTableModel(pc_cols, 0);
+		pctb = new JTable(defaultTable_pc) {
+			@Override
+			public Component prepareRenderer(TableCellRenderer renderer, int rowIndex, int columnIndex) {
+				Component componenet = super.prepareRenderer(renderer, rowIndex, columnIndex);
+				Object value = getModel().getValueAt(rowIndex, columnIndex);
+				if (rowIndex % 2 == 1) {
+					if (columnIndex == 2) {
+						if (value.equals("ON")) {
+							componenet.setFont(new Font("TimesRoman", Font.BOLD, 13));
+							componenet.setBackground(Color.lightGray);
+							componenet.setForeground(Color.GREEN);
+						}
+						if (value.equals("OFF")) {
+							// if date equal current date
+							componenet.setFont(new Font("TimesRoman", Font.BOLD, 13));
+							componenet.setBackground(Color.lightGray);
+							componenet.setForeground(Color.red);
+						}
+					} else {
+						componenet.setFont(new Font("TimesRoman", Font.BOLD, 13));
+						componenet.setBackground(Color.lightGray);
+						componenet.setForeground(Color.blue);
+					}
+				} else {
+					if (columnIndex == 2) {
+						if (value.equals("ON")) {
+							componenet.setFont(new Font("TimesRoman", Font.BOLD, 13));
+							componenet.setBackground(Color.white);
+							componenet.setForeground(Color.GREEN);
+						}
+						if (value.equals("OFF")) {
+							// if date equal current date
+							componenet.setFont(new Font("TimesRoman", Font.BOLD, 13));
+							componenet.setBackground(Color.white);
+							componenet.setForeground(Color.red);
+						}
+					} else {
+						componenet.setFont(new Font("TimesRoman", Font.BOLD, 13));
+						componenet.setBackground(Color.white);
+						componenet.setForeground(Color.blue);
+					}
+				}
+				return componenet;
+			}
+
+			public boolean isCellEditable(int rowIndex, int cellIndex) { // DISEDITABLE TABLE VALUES
+				return false;
+			}
+
+		};
+		pctb.setRowHeight(30);
+		pctb.setSize(1200, 480);
+		pctb.setLocation(5, 5);
+		JTableHeader tbhead = pctb.getTableHeader();
+		tbhead.setFont(new Font("TimesRoman", Font.BOLD, 16));
+		tbhead.setForeground(Color.GREEN);
+		tbhead.setBackground(Color.BLACK);
+		tbhead.setBorder(Constants.RED_BORDER);
+		pctb.setPreferredScrollableViewportSize(new Dimension(1100, 600));
+		pctb.setFillsViewportHeight(true);
+		JScrollPane js = new JScrollPane(pctb);
+		js.setSize(1170, 480);
+		js.setLocation(5, 5);
+		jtp = new JTabbedPane(JTabbedPane.TOP);
+		pc_pn = new JPanel(null);
+		pc_pn.setSize(1200, 675);
+		pc_pn.setLocation(0, 0);
+		pctb.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int row = pctb.rowAtPoint(e.getPoint());
+				if (row >= 0 && row < pctb.getRowCount()) {
+					pctb.setRowSelectionInterval(row, row);
+				} else {
+					pctb.clearSelection();
+				}
+				if (e.getButton() == MouseEvent.BUTTON1 && e.getClickCount() == 2) { // double click chuột trái để
+																						// start/stop pc được chọn.
+//					startbutton(row, "khach");
+				}
+				if (e.getButton() == MouseEvent.BUTTON3) { // nhấn chuột phải để hiện popup login
+//					loginbutton(row);
+				}
+			}
+		});
+		JLabel addPCbg = new JLabel(new ImageIcon("rsc/akempr.jpg"));
+		addPCbg.setSize(1200, 300);
+		addPCbg.setLocation(0, 485);
+		addPC_pn = new JPanel(null);
+		addPC_pn.setSize(1200, 200);
+		addPC_pn.setLocation(0, 0);
+		Icon addbtn = new ImageIcon("rsc/addbtn.png");
+		JButton addPC = new JButton(addbtn);
+		addPC.setSize(218, 50);
+		addPC.setLocation(10, 10);
+		addPC.setFont(new Font("TimesRoman", Font.BOLD, 15));
+		addPC.setBackground(Color.red);
+		addPC.setToolTipText("Click để thêm máy tính!");
+		addPC.setBackground(Color.PINK);
+		addPC.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+//				AddPCB(Integer.toString(defaultTable_pc.getRowCount()));
+			}
+		});
+		Icon delbtn = new ImageIcon("rsc/button.png");
+		delPC = new JButton(delbtn);
+		delPC.setSize(218, 50);
+		delPC.setLocation(228, 10);
+		delPC.setFont(new Font("TimesRoman", Font.BOLD, 15));
+		delPC.setBackground(Color.red);
+		delPC.setToolTipText("Click để xoá máy tính cuối cùng!");
+		delPC.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+//				DelButton();
+			}
+		});
+		Icon changebtn = new ImageIcon("rsc/changemoney.png");
+		JButton money_b = new JButton(changebtn);
+		money_b.setSize(218, 50);
+		money_b.setLocation(960, 5);
+		money_b.setFont(new Font("TimesRoman", Font.BOLD, 15));
+		money_b.setBackground(Color.red);
+		money_b.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+//				ChangeMoney();
+			}
+		});
+		addPCbg.add(addPC);
+		addPCbg.add(delPC);
+		addPCbg.add(money_b);
+		pc_pn.add(js);
+		pc_pn.add(addPCbg);
+
+		jtp.addTab("Máy trạm", pc_pn);
+		jf.getContentPane().add(jtp);
+		jf.setVisible(true);
+	}
 }
